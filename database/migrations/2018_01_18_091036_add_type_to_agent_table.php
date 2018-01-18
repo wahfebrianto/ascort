90332f16d2e417553c0445b35fbb7ea036a9a405
Wahyu <wahfebrianto@gmail.com>
2018-01-18 09:15:09 +0700

    add type column migration

---
+++ database/migrations/2018_01_18_091036_add_type_to_agent_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypeToAgentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('agents', function (Blueprint $table) {
+            $table->string('type')->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('agents', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+}
