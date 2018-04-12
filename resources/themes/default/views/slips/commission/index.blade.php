998af78be1dc45339f377f04d5371b70311f50ba
Wahyu <wahfebrianto@gmail.com>
2018-04-12 23:45:32 +0700

    Change date into between to date in commission slip

--- resources/themes/default/views/slips/commission/index.blade.php
+++ resources/themes/default/views/slips/commission/index.blade.php
@@ -36,15 +36,17 @@
                     <div class="border-decor"></div>
                     {!! Form::open( ['route' => 'slips.commission.export', 'id' => 'form_export_sales', 'method' => 'GET', 'class' => 'form-horizontal'] ) !!}
                     <div class="form-group">
-                        {!! Form::label('month', trans('slips/overriding/general.columns.date'), ['class' => 'control-label col-sm-4'] ) !!}
+                        {!! Form::label('start_date', trans('slips/overriding/general.columns.date'), ['class' => 'control-label col-sm-4'] ) !!}
                         <div class="col-sm-8">
-                            <div class="input-group">
-                                <span class="input-group-addon">period</span>
-                                {!! Form::select('period', ['1' => '1', '2' => '2', '3' => '3', '4' => '4'], '1', ['class' => 'form-control', 'id' => 'agent_position_id_select',  'style' => "width: 100%"]) !!}
-                                <span class="input-group-addon">month</span>
-                                {!! Form::select('month', $month_lists, date('m'), ['class' => 'form-control', 'id' => 'agent_position_id_select',  'style' => "width: 100%"]) !!}
-                                <span class="input-group-addon">year</span>
-                                {!! Form::select('year', $year_lists, date('Y'), ['class' => 'form-control', 'id' => 'agent_position_id_select',  'style' => "width: 100%"]) !!}
+                            <div class="input-group col-md-6 pull-left">
+                                <span class="input-group-addon">Dari</span>
+                                {!! Form::text('start_date', null, ['class' => 'form-control datepicker', 'data-date-format' => 'dd/mm/yyyy', 'tabindex' => 1]) !!}
+                                <span class = "input-group-addon"><span class="fa fa-fw fa-fw fa-calendar"></span>
+                            </div>
+                            <div class="input-group col-md-6 pull-right">
+                                <span class="input-group-addon">Hingga</span>
+                                {!! Form::text('end_date', null, ['class' => 'form-control datepicker', 'data-date-format' => 'dd/mm/yyyy', 'tabindex' => 2]) !!}
+                                <span class = "input-group-addon"><span class="fa fa-fw fa-fw fa-calendar"></span>
                             </div>
                         </div>
                     </div>
@@ -52,7 +54,7 @@
                         {!! Form::label('agent_id', trans('sales/general.columns.agent_id'), ['class' => 'control-label col-sm-4'] ) !!}
                         <div class="col-sm-8">
                             <div class="input-group select2-bootstrap-append">
-                                {!! Form::select('agent_id', ['all' => 'All'] + \App\Agent::getAgents_ForDropDown(), 'all', ['class' => 'form-control select2', 'style' => "width: 100%"]) !!}
+                                {!! Form::select('agent_id', ['all' => 'All'] + \App\Agent::getAgents_ForDropDown(), 'all', ['class' => 'form-control select2', 'style' => "width: 100%", 'tabindex' => 3]) !!}
                                 <span class="input-group-addon">
                                     <span class="fa fa-fw fa-fw fa-child"></span>
                                 </span>
@@ -63,7 +65,7 @@
                         {!! Form::label('agent_position_id', trans('agents/general.columns.agent_position_id'), ['class' => 'control-label col-sm-4'] ) !!}
                         <div class="col-sm-8">
                             <div class="input-group">
-                                {!! Form::select('agent_position_id', ['all' => 'All'] + $agent_position_lists, 'all', ['class' => 'form-control', 'id' => 'agent_position_id_select',  'style' => "width: 100%"]) !!}
+                                {!! Form::select('agent_position_id', ['all' => 'All'] + $agent_position_lists, 'all', ['class' => 'form-control', 'id' => 'agent_position_id_select',  'style' => "width: 100%", 'tabindex' => 4]) !!}
                                 <span class="input-group-addon">
                                     <span class="fa fa-fw fa-briefcase"></span>
                                 </span>
@@ -101,9 +103,11 @@
 @endsection
 
 @section('body_bottom')
+    <script src="{{ asset ("/bower_components/admin-lte/plugins/datepicker/bootstrap-datepicker.js") }}" type="text/javascript"></script>
     <script src="{{ asset ("/bower_components/admin-lte/select2/js/select2.min.js") }}" type="text/javascript"></script>
     <script>
         $(document).ready(function() {
+            $('.datepicker').datepicker().attr('placeholder', 'dd/mm/yyyy');
             $('.select2').select2();
         });
     </script>
