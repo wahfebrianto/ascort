b931682ff0a038c3a3f612b7af08046caf48811f
Wahyu <wahfebrianto@gmail.com>
2018-07-18 04:02:08 +0700

    Fix Template 3 Excel Export

--- app/Http/Requests/AgentsExcelExportHandler.php
+++ app/Http/Requests/AgentsExcelExportHandler.php
@@ -2,101 +2,68 @@
 namespace App\Http\Requests;
 use Maatwebsite\Excel\Files\ExportHandler;
 
-class AgentsExcelExportHandler implements ExportHandler
-{
+class AgentsExcelExportHandler implements ExportHandler {
 
     public function handle($file)
-    {
-        $ctr = 1;
+    {$ctr=1;
         // change all data that need to be formatted
-        $dataSheet1 = $this->getSheetData1($file->data);
-        $dataSheet2 = $this->getSheetData2($file->data);
-        $dataSheet3 = $this->getSheetData3($file->data);
-        $dataSheet4 = $this->getSheetData4($file->data);
-
-        // work on the exportAgentsExcelExport
-        return Excel::create('RekapKomisiMingguan', function($file) {
-
-            // Our first sheet
-            $file->sheet('Summary Komisi', function($sheet) use ($file)
-            {
-                $columnCount = count($file->data[0]);
-                $sheet->cells('1', function($cells)
-                {
-                    $cells->setFontSize(12);
-                    $cells->setFontWeight('bold');
-                });
-                $sheet->setBorder('A1:' . chr(64 + $columnCount) . strval(sizeof($file->data) + 1), 'solid');
-                $sheet->setFontSize(12);
-                if (config('global.export_type') == 'pdf') {
-                    $sheet->setOrientation('landscape');
+        foreach($file->data as &$datum) {
+			
+            foreach ($datum as $key => &$value) {
+				//dd($datum);
+                if ($key == 'agent_position_id') {
+                    $value = $datum['agent_position']['name'];
+                    // change key
+                    $datum['agent_position'] = $value;
+                    unset($datum[$key]);
+                } elseif($key == 'gender') {
+                    $value = trans('general.gender.' . $value);
+                } elseif($key == 'DOB') {
+                    //$value = \DateTime::createFromFormat('Y-m-d', $value)->format('m/d/Y'); // excel format
+					$value = date_format(date_create($value),'m/d/Y');
+                } elseif($key == 'id_card_expiry_date') {
+                    //$value = \DateTime::createFromFormat('Y-m-d', $value)->format('m/d/Y'); // excel format
+					$value = date_format(date_create($value),'m/d/Y');
                 }
-                $sheet->fromArray($file->data, null, 'A1', true, true);
-                $sheet->setPaperSize(\PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3);
-                $sheet->cells('A1:' . chr(64 + $columnCount) . '1', function($cells)
-                {
-                    //$cells->setFontSize(8);
-                    $cells->setBackground('#AAAAAA');
-                    $cells->setAlignment('center');
-                });
-            });
+				else if($key == 'branch_office_id')
+				{
+					$value = \App\BranchOffice::getBranchOfficeFromId($datum['branch_office_id'])->branch_name;
+				}
+				else if($key == 'parent_id')
+				{
+					$value = $datum['parent_name'];
+				}
+            }
+			unset($datum["parent_name"]);
+			//unset($datum["agent_position_name"]);
+			$columnLang = trans('agents/general.columns');
+			$newdatum = array();
+			foreach($datum as $key => $value){
+				$newdatum[$columnLang[$key]] = $datum[$key];
+			}
+			$datum = array('No'=>$ctr++);
+			$datum = array_merge($datum,$newdatum);
 
-            // Our second sheet
-            $excel->sheet('Second sheet', function($sheet) {
-
-            });
-
-        })->export('xls');
-        return $file->sheet('agents', function($sheet) use ($file)
+        }
+        // work on the exportAgentsExcelExport
+        return $file->sheet('agents', function($sheet) use($file)
         {
-            $columnCount = count($file->data[0]);
-            $sheet->cells('1', function($cells)
-            {
+			$columnCount = count($file->data[0]);
+            $sheet->cells('1', function($cells) {
                 $cells->setFontSize(12);
                 $cells->setFontWeight('bold');
             });
-            $sheet->setBorder('A1:' . chr(64 + $columnCount) . strval(sizeof($file->data) + 1), 'solid');
-            $sheet->setFontSize(12);
-            if (config('global.export_type') == 'pdf') {
-                $sheet->setOrientation('landscape');
-            }
+			$sheet->setBorder('A1:'.chr(64+$columnCount).strval(sizeof($file->data)+1),'solid');
+			$sheet->setFontSize(12);
+			if(config('global.export_type') == 'pdf'){$sheet->setOrientation('landscape');}
             $sheet->fromArray($file->data, null, 'A1', true, true);
-            $sheet->setPaperSize(\PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3);
-            $sheet->cells('A1:' . chr(64 + $columnCount) . '1', function($cells)
-            {
+			$sheet->setPaperSize(\PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3);
+			$sheet->cells('A1:'.chr(64+$columnCount).'1', function($cells) {
                 //$cells->setFontSize(8);
-                $cells->setBackground('#AAAAAA');
-                $cells->setAlignment('center');
+				$cells->setBackground('#AAAAAA');
+				$cells->setAlignment('center');
             });
         })->export(config('global.export_type'));
     }
 
-    private function getSheetData1($data)
-    {
-      $result = [];
-      $ctr = 1;
-      foreach ($data as $datum) {
-          $rowData = [];
-          $rowData["No"] = $ctr;
-          $rowData["Nama Investor"] = $datum->sales;
-          $rowData["no"] = $ctr;
-          $result[] = $rowData;
-      }
-      return $result;
-    }
-
-    private function getSheetData2($data)
-    {
-
-    }
-
-    private function getSheetData3($data)
-    {
-
-    }
-
-    private function getSheetData4($data)
-    {
-
-    }
 }
--- app/Http/Requests/RecapExcelExportHandler.php
+++ app/Http/Requests/RecapExcelExportHandler.php
@@ -1,38 +1,105 @@
 <?php
 namespace App\Http\Requests;
-
 use Maatwebsite\Excel\Files\ExportHandler;
-use Carbon\Carbon;
 
-class RecapExcelExportHandler implements ExportHandler {
+class RecapExcelExportHandler implements ExportHandler
+{
 
     public function handle($file)
     {
+        $ctr = 1;
         // change all data that need to be formatted
-        foreach($file->data as &$datum) {
-            foreach ($datum as $key => &$value) {
-                if ($key == 'agent_id') {
-                    $value = $datum['agent']['name'];
-                    // change key
-                    $datum['agent_name'] = $value;
-                    $datum['agent_position_name'] = \App\AgentPosition::where('id', '=', $datum['agent']['agent_position_id'])->first()->name;
-                    unset($datum[$key]);
-                } else if($key == 'process_date') {
-                    $value = Carbon::parse($value)->format('m/d/Y'); // excel format
-                } else if(in_array($key, ['total_nominal', 'total_FYP', 'minus', 'cuts', 'commission_hold', 'created_at', 'updated_at', 'data'])) {
-                    unset($datum[$key]);
-                }
-            }
-        }
+        $dataSheet1 = $this->getSheetData1($file->data);
+        $dataSheet2 = $this->getSheetData2($file->data);
+        $dataSheet3 = $this->getSheetData3($file->data);
+        $dataSheet4 = $this->getSheetData4($file->data);
+
         // work on the exportAgentsExcelExport
-        return $file->sheet('agents', function($sheet) use($file)
+        return Excel::create('RekapKomisiMingguan', function($file) {
+
+            // Our first sheet
+            $file->sheet('Summary Komisi', function($sheet) use ($file)
+            {
+                $columnCount = count($file->data[0]);
+                $sheet->cells('1', function($cells)
+                {
+                    $cells->setFontSize(12);
+                    $cells->setFontWeight('bold');
+                });
+                $sheet->setBorder('A1:' . chr(64 + $columnCount) . strval(sizeof($file->data) + 1), 'solid');
+                $sheet->setFontSize(12);
+                if (config('global.export_type') == 'pdf') {
+                    $sheet->setOrientation('landscape');
+                }
+                $sheet->fromArray($file->data, null, 'A1', true, true);
+                $sheet->setPaperSize(\PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3);
+                $sheet->cells('A1:' . chr(64 + $columnCount) . '1', function($cells)
+                {
+                    //$cells->setFontSize(8);
+                    $cells->setBackground('#AAAAAA');
+                    $cells->setAlignment('center');
+                });
+            });
+
+            // Our second sheet
+            $excel->sheet('Second sheet', function($sheet) {
+
+            });
+
+        })->export('xls');
+        return $file->sheet('agents', function($sheet) use ($file)
         {
-            $sheet->cells('1', function($cells) {
+            $columnCount = count($file->data[0]);
+            $sheet->cells('1', function($cells)
+            {
                 $cells->setFontSize(12);
                 $cells->setFontWeight('bold');
             });
+            $sheet->setBorder('A1:' . chr(64 + $columnCount) . strval(sizeof($file->data) + 1), 'solid');
+            $sheet->setFontSize(12);
+            if (config('global.export_type') == 'pdf') {
+                $sheet->setOrientation('landscape');
+            }
             $sheet->fromArray($file->data, null, 'A1', true, true);
-        })->export('xlsx');
+            $sheet->setPaperSize(\PHPExcel_Worksheet_PageSetup::PAPERSIZE_A3);
+            $sheet->cells('A1:' . chr(64 + $columnCount) . '1', function($cells)
+            {
+                //$cells->setFontSize(8);
+                $cells->setBackground('#AAAAAA');
+                $cells->setAlignment('center');
+            });
+        })->export(config('global.export_type'));
     }
 
+    private function getSheetData1($data)
+    {
+
+    }
+
+    private function getSheetData2($data)
+    {
+
+    }
+
+    private function getSheetData3($data)
+    {
+      $result = [];
+      $ctr = 1;
+      foreach ($data as $agent) {
+        foreach ($agent->sales as $sale) {
+          $rowData = [];
+          $rowData["No"] = $ctr;
+          $rowData["Nama Investor"] = $datum->sales;
+          $rowData["no"] = $ctr;
+          $result[] = $rowData;
+          $ctr++;
+        }
+      }
+      return $result;
+    }
+
+    private function getSheetData4($data)
+    {
+
+    }
 }
